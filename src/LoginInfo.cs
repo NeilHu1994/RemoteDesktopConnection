47e622c4633d0426699778d70d7436f75ab8241b
NeilHu <826132862@qq.com>
2019-12-22 22:40:57 +0800

    modify folder structure

---
+++ src/LoginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Telsafe
+{
+    class LoginInfo
+    {
+        public string Ipaddress { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
