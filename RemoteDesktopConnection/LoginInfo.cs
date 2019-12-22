026a89d4d87c19fa3aebf3e74542e8bd50dbe53e
NeilHu <826132862@qq.com>
2019-12-22 22:28:03 +0800

    modify folder structure

--- RemoteDesktopConnection/LoginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RemoteDesktopConnection
-{
-    class LoginInfo
-    {
-        public string Ipaddress { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
